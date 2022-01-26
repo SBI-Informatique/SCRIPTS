593f3e559b24fae30a0b20c362f97ea2c4f2ff9d
SBI-Informatique <42469867+SBI-Informatique@users.noreply.github.com>
2022-01-26 15:19:08 +0100

    Update LISTE_COMPTE_ADMINISTRATEURS.ps1

--- LISTE_COMPTE_ADMINISTRATEURS.ps1
+++ LISTE_COMPTE_ADMINISTRATEURS.ps1
@@ -1,2 +1,1 @@
-New-Item -Path 'C:\sbi\LISTE_COMPTE_ADMINISTRATEURS.txt' -ItemType File
 Get-LocalGroupMember -Group "Administrateurs" | Select-Object Name | Format-Table -HideTableHead > C:\SBI\LISTE_COMPTE_ADMINISTRATEURS.txt
