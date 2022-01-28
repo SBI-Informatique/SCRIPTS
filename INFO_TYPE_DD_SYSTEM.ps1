340427753e0fd688e77b3957d86ce67b9c4f3391
SBI-Informatique <42469867+SBI-Informatique@users.noreply.github.com>
2022-01-28 17:19:09 +0100

    Update INFO_TYPE_DD_SYSTEM.ps1

--- INFO_TYPE_DD_SYSTEM.ps1
+++ INFO_TYPE_DD_SYSTEM.ps1
@@ -1,1 +1,8 @@
-get-physicaldisk | Select-Object MediaType | Format-Table -HideTableHead
+$drive_type = (Get-PhysicalDisk | Select "MediaType").MediaType.Contains("SSD")
+
+if($drive_type)
+{
+write-host "SSD `r`n"
+} else {
+write-host "HDD `r`n"
+}
