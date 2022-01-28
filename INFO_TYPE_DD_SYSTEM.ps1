311517e79866965372e80ace5c04e524dfc48b48
SBI-Informatique <42469867+SBI-Informatique@users.noreply.github.com>
2022-01-28 20:19:03 +0100

    Update INFO_TYPE_DD_SYSTEM.ps1

--- INFO_TYPE_DD_SYSTEM.ps1
+++ INFO_TYPE_DD_SYSTEM.ps1
@@ -1,10 +1,9 @@
 #Test de la version de Windows
-$version= (Get-CimInstance Win32_OperatingSystem).version
+$version= (Get-WmiObject Win32_OperatingSystem).Version
 $length= $version.Length
 $index= $version.IndexOf(".")
 [int]$windows= $version.Remove($index,$length-2)  
 
-#WINDOWS 10
 IF ($windows -eq 10) {
 $drive_type = (Get-PhysicalDisk | Select "MediaType").MediaType.Contains("SSD")
     if($drive_type) {
