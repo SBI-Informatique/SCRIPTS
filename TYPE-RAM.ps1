90c80e415f2c4ad01447f94ca8e6f3a000b16a0e
SBI-Informatique <42469867+SBI-Informatique@users.noreply.github.com>
2022-01-29 00:13:10 +0100

    Update TYPE-RAM.ps1

--- TYPE-RAM.ps1
+++ TYPE-RAM.ps1
@@ -1,13 +1,2 @@
-#Test de la version de Windows
-$version= (Get-WmiObject Win32_OperatingSystem).Version
-$length= $version.Length
-$index= $version.IndexOf(".")
-[int]$windows= $version.Remove($index,$length-2)  
-
-IF ($windows -eq 10) {
 (Get-WmiObject Win32_PhysicalMemory).PartNumber | Format-Table -HideTableHeaders
 exit 1
-} ELSE {
-write-host ""
-exit 0
-}
