0b211b9b6c908f791fa276ecbfad98830a6e7fbc
SBI-Informatique <42469867+SBI-Informatique@users.noreply.github.com>
2022-01-28 20:12:14 +0100

    Update INFO_TYPE_DD_SYSTEM.ps1

--- INFO_TYPE_DD_SYSTEM.ps1
+++ INFO_TYPE_DD_SYSTEM.ps1
@@ -1,8 +1,17 @@
-$drive_type = (Get-PhysicalDisk | Select "MediaType").MediaType.Contains("SSD")
+#Test de la version de Windows
+$version= (Get-CimInstance Win32_OperatingSystem).version
+$length= $version.Length
+$index= $version.IndexOf(".")
+[int]$windows= $version.Remove($index,$length-2)  
 
-if($drive_type)
-{
-write-host "SSD `r`n"
-} else {
-write-host "HDD `r`n"
+#WINDOWS 10
+IF ($windows -eq 10) {
+$drive_type = (Get-PhysicalDisk | Select "MediaType").MediaType.Contains("SSD")
+    if($drive_type) {
+    write-host "SSD `r`n"
+    } ELSE {
+    write-host "HDD `r`n"
+    }
+} ELSE {
+write-host ""
 }
