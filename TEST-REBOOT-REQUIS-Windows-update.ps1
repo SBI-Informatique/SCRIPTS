ef6fb4f554a9a643df914af4061c9e25160345a1
SBI-Informatique <42469867+SBI-Informatique@users.noreply.github.com>
2022-01-29 11:50:34 +0100

    Update TEST-REBOOT-REQUIS-Windows-update.ps1

--- TEST-REBOOT-REQUIS-Windows-update.ps1
+++ TEST-REBOOT-REQUIS-Windows-update.ps1
@@ -1,1 +1,28 @@
 test-path -path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired'
+
+#Test de la version de Windows
+$version= (Get-WmiObject Win32_OperatingSystem).Version
+$length= $version.Length
+$index= $version.IndexOf(".")
+[int]$windows= $version.Remove($index,$length-2)  
+
+IF ($windows -eq 10) {
+$TEST-REGISTRE = test-path -path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired'
+    if($TEST-REGISTRE) {
+    write-host "REBOOT-REQUIS"
+    exit 1
+    } ELSE {
+    write-host "OK"
+    exit 0
+    }
+} ELSE {
+$TEST-REGISTRE = test-path -path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired'
+    if($TEST-REGISTRE) {
+    write-host "REBOOT-REQUIS"
+    exit 1
+    } ELSE {
+    write-host "OK"
+    exit 0
+    }
+}
+}
