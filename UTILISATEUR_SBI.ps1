d7b32fb788f309b9ff8acbcf95d9cc014f311870
SBI-Informatique <42469867+SBI-Informatique@users.noreply.github.com>
2022-02-02 00:45:49 +0100

    Update UTILISATEUR_SBI.ps1

--- UTILISATEUR_SBI.ps1
+++ UTILISATEUR_SBI.ps1
@@ -1,5 +1,16 @@
-New-LocalUser -Name $param1 -Description "Son of Goku" -Password $param2
 
-net user %1 %2 /add
-net user %1 /active:yes
-net localgroup administrateurs %1 /add
+#Test de la version de Windows
+$version= (Get-WmiObject Win32_OperatingSystem).Version
+$length= $version.Length
+$index= $version.IndexOf(".")
+[int]$windows= $version.Remove($index,$length-2)  
+
+IF ($windows -eq 10) {
+New-LocalUser -Name $param1 -Description "Son of Goku" -Password $param2
+exit 1
+} ELSE {
+cmd.exe /c net user $param1 $param2 /add
+cmd.exe /c net user $param1 /active:yes
+cmd.exe /c net localgroup administrateurs $param1 /add
+exit 1
+}
