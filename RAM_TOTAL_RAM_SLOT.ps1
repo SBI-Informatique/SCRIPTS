ab22f4a0262849846dbcbb8bb30dd8a6b2b199de
SBI-Informatique <42469867+SBI-Informatique@users.noreply.github.com>
2022-02-18 11:58:47 +0100

    Update RAM_TOTAL_RAM_SLOT.ps1

--- RAM_TOTAL_RAM_SLOT.ps1
+++ RAM_TOTAL_RAM_SLOT.ps1
@@ -1,1 +1,12 @@
+$TYPE_OS = (get-wmiobject -computer LocalHost win32_computersystem).Model
+$TYPE_OS_VIRTUEL = $false
+
+IF ($TYPE_OS = "Virtual Machine" -or $TYPE_OS -eq "VMware7,1") {
+$TYPE_OS_VIRTUEL = $True
+}
+
+IF ($TYPE_OS_VIRTUEL = $False ) {
 powershell.exe "(Get-WmiObject Win32_physicalMemoryArray).MemoryDevices"
+} ELSE {
+Write-Host "Virtuel"
+}
