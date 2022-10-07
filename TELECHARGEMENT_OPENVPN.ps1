2083fcde9370d3d734e90ec84c9720e379461476
SBI-Informatique <42469867+SBI-Informatique@users.noreply.github.com>
2022-10-07 14:45:09 +0200

    Update TELECHARGEMENT_OPENVPN.ps1

--- TELECHARGEMENT_OPENVPN.ps1
+++ TELECHARGEMENT_OPENVPN.ps1
@@ -1,16 +1,65 @@
-$path = "C:\SBI\TICKET"
-$TargetFile = "https://sbi-informatique.zendesk.com/hc/fr/requests/new"
-$ShortcutFile = "C:\Users\Public\Desktop\Ticket-SBI.lnk"
-$WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut($ShortcutFile)
+#--------------------------------------------------------------------------------------------------------------------------
+#1/ VARIABLES :
+#--------------------------------------------------------------------------------------------------------------------------
+# VARIABLES
+    $PATH_OPENVPN = "C:\SBI\OPENVPN"
+    $PATH_OPENVPN_MSI = "C:\SBI\OPENVPN\OPENVPN.msi"
+    $PATH_OPENVPN_ZIP = "C:\SBI\OPENVPN\OPENVPN.zip"
+    $PATH_7ZIP_PROGRAMME = "C:\Program Files\7-Zip\7z.exe"
 
-If(!(test-path $path))
+#--------------------------------------------------------------------------------------------------------------------------
+#2/ TELECHARGEMENT DE LA SOURCE :
+#--------------------------------------------------------------------------------------------------------------------------
+If(!(test-path $PATH_OPENVPN))
 {
-mkdir C:\SBI\TICKET
-}
-    Import-Module BitsTransfer
-    Start-BitsTransfer http://sbiinforkw.cluster020.hosting.ovh.net/Applications/TICKET/SBI_LOGO_TICKET.ico C:/SBI/TICKET/SBI_LOGO_TICKET.ico
+    #1/ Création du répertoire OPENVPN
+    mkdir C:\SBI\OPENVPN
 
-    $Shortcut.TargetPath = $TargetFile
-    $Shortcut.IconLocation = "C:/SBI/TICKET/SBI_LOGO_TICKET.ico";
-    $Shortcut.Save()
+    #2/ Création du répertoire OPENVPN
+    mkdir C:\SBI\7ZIP
+   
+    #3/ Import du module BITSTRANSFER
+    Import-Module BitsTransfer
+
+    #4/ Telechargement de OPENVPN
+    Start-BitsTransfer http://sbiinforkw.cluster020.hosting.ovh.net/Applications/OPENVPN/OPENVPN.zip C:/SBI/OPENVPN/OPENVPN.zip
+
+    #5/ Telechargement de 7ZIP
+    Start-BitsTransfer http://sbiinforkw.cluster020.hosting.ovh.net/Applications/7ZIP/7zip.exe C:/SBI/7ZIP/7zip.exe
+}
+
+#--------------------------------------------------------------------------------------------------------------------------
+#3/ INSTALLATION DU PROVIDER (nuget)
+#--------------------------------------------------------------------------------------------------------------------------
+If(!(Get-PackageProvider -ListAvailable -Name Nuget)) # Est-ce que le provider existe déja ?
+    {
+        Install-PackageProvider -Name Nuget -MinimumVersion 2.8.5.201 -Force
+    }
+
+#--------------------------------------------------------------------------------------------------------------------------
+#4/ INSTALLATION DE 7ZIP (module Powershell) :
+#--------------------------------------------------------------------------------------------------------------------------
+If(!(Get-Module -ListAvailable -Name 7Zip4Powershell)) # Est-ce que le module existe déja ?
+    {
+        Install-Module 7Zip4PowerShell -Force
+    }
+
+#--------------------------------------------------------------------------------------------------------------------------
+#5/ EXTRACTION OPENVPN.ZIP :
+#--------------------------------------------------------------------------------------------------------------------------
+If((test-path $PATH_OPENVPN_ZIP))
+    {
+        #3/ Extraction du fichier ZIP OPENVPN
+        Expand-7Zip -ArchiveFileName $PATH_OPENVPN_ZIP -TargetPath $PATH_OPENVPN
+        rm $PATH_OPENVPN_ZIP
+    }
+
+#--------------------------------------------------------------------------------------------------------------------------
+#6/ INSTALATION DE OPENVPN :
+#--------------------------------------------------------------------------------------------------------------------------
+ If((test-path $PATH_OPENVPN_MSI))
+    {
+        #3/ Extraction du fichier ZIP OPENVPN
+        msiexec.exe /i $PATH_OPENVPN_MSI /qn
+    }
+#--------------------------------------------------------------------------------------------------------------------------
