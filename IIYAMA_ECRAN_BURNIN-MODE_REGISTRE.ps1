448a6f54b689602e295bb2aedc95a2f5fca948cd
SBI-Informatique <42469867+SBI-Informatique@users.noreply.github.com>
2023-01-05 13:13:35 +0100

    Update IIYAMA_ECRAN_BURNIN-MODE_REGISTRE.ps1

--- IIYAMA_ECRAN_BURNIN-MODE_REGISTRE.ps1
+++ IIYAMA_ECRAN_BURNIN-MODE_REGISTRE.ps1
@@ -1,2 +1,6 @@
 Set-Itemproperty -path 'HKLM:\SYSTEM\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0001' -Name 'AdaptiveVsyncEnable' -type 'DWORD' -value '0'
 Set-Itemproperty -path 'HKLM:\SYSTEM\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000' -Name 'AdaptiveVsyncEnable' -type 'DWORD' -value '0'
+Set-Itemproperty -path 'HKLM:\SYSTEM\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0002' -Name 'AdaptiveVsyncEnable' -type 'DWORD' -value '0'
+Set-Itemproperty -path 'HKLM:\SYSTEM\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0003' -Name 'AdaptiveVsyncEnable' -type 'DWORD' -value '0'
+Set-Itemproperty -path 'HKLM:\SYSTEM\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0004' -Name 'AdaptiveVsyncEnable' -type 'DWORD' -value '0'
+Set-Itemproperty -path 'HKLM:\SYSTEM\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0005' -Name 'AdaptiveVsyncEnable' -type 'DWORD' -value '0'
