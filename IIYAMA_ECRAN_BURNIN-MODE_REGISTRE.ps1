bee993b0eb2e6d596697615aefb797c79c721aa3
SBI-Informatique <42469867+SBI-Informatique@users.noreply.github.com>
2023-01-05 10:49:51 +0100

    Update IIYAMA_ECRAN_BURNIN-MODE_REGISTRE.ps1

--- IIYAMA_ECRAN_BURNIN-MODE_REGISTRE.ps1
+++ IIYAMA_ECRAN_BURNIN-MODE_REGISTRE.ps1
@@ -1,2 +1,2 @@
-Set-Itemproperty -path 'HKLM:\SYSTEM\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0001' -Name 'AdaptiveVsyncEnable' -value '0'
-Set-Itemproperty -path 'HKLM:\SYSTEM\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000' -Name 'AdaptiveVsyncEnable' -value '0'
+Set-Itemproperty -path 'HKLM:\SYSTEM\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0001' -Name 'AdaptiveVsyncEnable' type 'DWORD' -value '0'
+Set-Itemproperty -path 'HKLM:\SYSTEM\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000' -Name 'AdaptiveVsyncEnable' type 'DWORD' -value '0'
