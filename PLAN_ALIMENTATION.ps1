0d80bce0d381e8bd9fda9221a9d7bce7cd24f6a6
SBI-Informatique <42469867+SBI-Informatique@users.noreply.github.com>
2023-02-13 11:54:10 +0100

    Update PLAN_ALIMENTATION.ps1

--- PLAN_ALIMENTATION.ps1
+++ PLAN_ALIMENTATION.ps1
@@ -5,14 +5,10 @@ $valueName = "ALIMENTATION-FULL"
 $value = Get-ItemProperty -Path $registryKey -Name $valueName
 
 if ($value.$valueName -eq 1) {
-  Get-WmiObject -Class "Win32PowerPlan" | ForEach-Object {
-      $plan = $
-      if ($plan.IsActive -eq $true) {
-          $guid = [guid]$plan.InstanceID
-          powercfg -setdcvalueindex $guid.ToString() 4f971e89-eebd-4455-a8de-9e59040e7347 3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e 0
-          powercfg -setacvalueindex $guid.ToString() 4f971e89-eebd-4455-a8de-9e59040e7347 3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e 0
-      }
-  }
+    powercfg -change -standby-timeout-ac 0
+    powercfg -change -hibernate-timeout-ac 0
+    powercfg -change -standby-timeout-dc 0
+    powercfg -change -hibernate-timeout-dc 0
     Write-Output "Plan d'alimentation désactivé"
 } else {
     Write-Output "Clée de registre non renseignée"
