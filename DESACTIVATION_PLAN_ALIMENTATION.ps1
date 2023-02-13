30b135d80a9858e96863c636ff700a11e0be273a
SBI-Informatique <42469867+SBI-Informatique@users.noreply.github.com>
2023-02-13 11:14:36 +0100

    Create DESACTIVATION_PLAN_ALIMENTATION.ps1

---
+++ DESACTIVATION_PLAN_ALIMENTATION.ps1
@@ -0,0 +1,28 @@
+
+$registryKey = "HKLM:\SOFTWARE\SBI\WINDOWS"
+$valueName = "ALLIMENTATION-FULL"
+
+$value = Get-ItemProperty -Path $registryKey -Name $valueName
+
+if ($value.$valueName -eq 1) {
+  Get-WmiObject -Class "Win32PowerPlan" | ForEach-Object {
+      $plan = $
+      if ($plan.IsActive -eq $true) {
+          $guid = [guid]$plan.InstanceID
+          powercfg -setdcvalueindex $guid.ToString() 4f971e89-eebd-4455-a8de-9e59040e7347 3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e 0
+          powercfg -setacvalueindex $guid.ToString() 4f971e89-eebd-4455-a8de-9e59040e7347 3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e 0
+      }
+  }
+    Write-Output "Plan d'alimentation désactivé"
+} else {
+    Write-Output "Clée de registre non renseignée"
+}
+
+<#
+CREE LA CLEE SBI
+  new-item -Path "HKLM:\SOFTWARE" -Name "SBI"
+CREE LA CLEE WINDOWS DANS SBI
+  new-item -Path "HKLM:\SOFTWARE\SBI" -Name "WINDOWS"
+AJOUTER LA CLEE DWORD ALLIMENTATION-FULL
+  New-ItemProperty -Path "HKLM:\SOFTWARE\SBI\WINDOWS" -Name "ALLIMENTATION-FULL" -Value "1" -PropertyType "DWORD"
+#>
