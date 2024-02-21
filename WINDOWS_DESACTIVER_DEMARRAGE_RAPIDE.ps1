4ce875312aa1d22e306e54d3182e4250b6be7fc3
SBI-Informatique <42469867+SBI-Informatique@users.noreply.github.com>
2024-02-21 12:18:19 +0100

    Update WINDOWS_DESACTIVER_DEMARRAGE_RAPIDE.ps1

--- WINDOWS_DESACTIVER_DEMARRAGE_RAPIDE.ps1
+++ WINDOWS_DESACTIVER_DEMARRAGE_RAPIDE.ps1
@@ -3,6 +3,9 @@ $registryPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power"
 # Nom de la propriété à vérifier
 $propertyName = "HiberbootEnabled"
 
+# Desactivation demarrage rapide
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power" -Name HiberbootEnabled -Value 0
+
 # Vérification de l'existence de la clé de registre et récupération de sa valeur
 if (Test-Path $registryPath) {
     $propertyValue = (Get-ItemProperty -Path $registryPath -Name $propertyName).$propertyName
